6b1b9f178082b0e7a921e1185e58e169ec60414c
mcxiaoke <mcxiaoke@gmail.com>
2016-06-07 14:01:06 +0800

    geo photos: init app project

---
+++ GeoPhotos/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  GeoPhotos
+//
+//  Created by mcxiaoke on 16/6/7.
+//  Copyright © 2016年 mcxiaoke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+  
+  var mainWindowController:MainWindowController?
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    let mwc = MainWindowController()
+    mwc.showWindow(self)
+    self.mainWindowController = mwc
+  }
+  
+  func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+    return true
+  }
+
+}
+
---
+++ GeoPhotos/MainWindowController.swift
@@ -0,0 +1,17 @@
+//
+//  MainWindowController.swift
+//  GeoPhotos
+//
+//  Created by mcxiaoke on 16/6/7.
+//  Copyright © 2016年 mcxiaoke. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+  override var windowNibName: String?{
+    return "MainWindowController"
+  }
+    
+}
