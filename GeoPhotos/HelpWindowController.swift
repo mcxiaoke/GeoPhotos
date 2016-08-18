029edea04385bce573c60721a8e4a4939248fe1e
mcxiaoke <mcxiaoke@gmail.com>
2016-08-18 11:14:11 +0800

    add empty help controller

---
+++ GeoPhotos/HelpWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  HelpWindowController.swift
+//  GeoPhotos
+//
+//  Created by mcxiaoke on 16/8/18.
+//  Copyright © 2016年 mcxiaoke. All rights reserved.
+//
+
+import Cocoa
+
+class HelpWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
